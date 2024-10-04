d4d42cf0d517636799ebf0da122b81777f812dca
sagar shinde <shindesagar01677@gmail.com>
2024-10-04 17:42:57 +0530

    new

--- src/Components/LoginForm.jsx
+++ src/Components/LoginForm.jsx
@@ -1,7 +1,9 @@
 import './LoginForm.css'
 import Data from '../Components/Data.json';
 import { useState } from 'react';
-import Screen from './Screen';
+import Screen1 from './Screen';
+
+let leftlogo = `${Data.leftlogo}`
 
 const LoginForm = () => {
   const [userdetails, setUserDetails] = useState({
@@ -11,12 +13,16 @@ const LoginForm = () => {
     email: ''
   })
   const [nameError, setNameError] = useState('');
+  const [LastnameError, setLastNameError] = useState('');
   const [mobileError, setMobileError] = useState('');
   const [emailError, setEmailError] = useState('');
   const [submited, setSubmited] = useState(false)
   const [modal, Setshowmodal] = useState(false)
   const [userexists, setUserExistsError] = useState('')
   const [zealid, setZealid] = useState('')
+  const [qrimg, setQrimg] = useState('')
+  const [IsModalVisible, setIsModalVisible] = useState(false);
+  const [isLoading, setIsLoading] = useState(false)
 
 
   // Validate user inputs
@@ -26,6 +32,16 @@ const LoginForm = () => {
       return false;
     } else {
       setNameError('');
+      return true;
+    }
+  };
+
+  const validateLastName = (lname) => {
+    if (lname.length < 2) {
+      setLastNameError('Please enter a valid last name.');
+      return false;
+    } else {
+      setLastNameError('');
       return true;
     }
   };
@@ -59,28 +75,32 @@ const LoginForm = () => {
       [name]: value
     }));
     if (name === 'fname') validateName(value);
+    if (name === 'lname') validateLastName(value); // Validate last name
     if (name === 'mobileno') validateMobile(value);
     if (name === 'email') validateEmail(value);
   };
 
   const formSubmit = async (e) => {
+
     e.preventDefault();
 
     const isNameValid = validateName(userdetails.fname);
+    const isLastNameValid = validateLastName(userdetails.lname);
     const isMobileValid = validateMobile(userdetails.mobileno);
     const isEmailValid = validateEmail(userdetails.email);
 
-    if (isNameValid && isMobileValid && isEmailValid) {
+    if (isNameValid && isLastNameValid && isMobileValid && isEmailValid) {
       try {
+        const eid = `${Data.eid}`;
         const formData = new FormData();
         formData.append('first_name', userdetails.fname);
         formData.append('last_name', userdetails.lname);
         formData.append('contact', userdetails.mobileno);
         formData.append('email', userdetails.email);
-        formData.append('eid', 'test1');
+        formData.append('eid', eid);
         formData.append('type', 'val');
 
-        const response = await fetch('http://192.168.1.25/Zeal_Event/API/index.php', {
+        const response = await fetch('https://www.eventsapp.co.in/registration/API/register.php', {
           method: 'POST',
           body: formData, // FormData automatically sets the correct content type
         });
@@ -88,7 +108,6 @@ const LoginForm = () => {
         const data = await response.json();
         console.log('Response data:', data);
         setZealid(data.Zeal_id);
-
         console.log(data)
 
         if (data.Exists === true) {
@@ -98,7 +117,7 @@ const LoginForm = () => {
         } else {
           // Proceed if user does not exist
           Setshowmodal(true); // Show confirmation modal
-          
+
         }
 
       } catch (error) {
@@ -113,78 +132,165 @@ const LoginForm = () => {
   }
 
 
+  const normalRegister = async () => {
+    setIsLoading(true);
+    try {
+      const eid = `${Data.eid}`;
+      const formData = new FormData();
+      formData.append('first_name', userdetails.fname);
+      formData.append('last_name', userdetails.lname);
+      formData.append('contact', userdetails.mobileno);
+      formData.append('email', userdetails.email);
+      formData.append('eid', eid);
+      formData.append('type', 'all');
+
+      const response = await fetch('http://192.168.1.25/Zeal_Event/API/register.php', {
+        method: 'POST',
+        body: formData, // FormData automatically sets the correct content type
+      });
+
+      const data = await response.json();
+      console.log('Response data:', data);
+      setZealid(data.Zeal_id);
+      console.log(data)
+      setQrimg(data.QR_FilePath);
+      if (data.Store === true) {
+        setIsModalVisible(true);
+        setIsLoading(false);
+      }
+
+    } catch (error) {
+      console.error('Error:', error);
+    }
+  }
+  const showform = () => {
+    setUserExistsError(false);
+  }
+
+
+  const downloadQr = () => {
+    if (qrimg) {
+      const link = document.createElement('a');
+      link.href = `http://192.168.1.25/Zeal_Event/API/${qrimg}`;
+      link.download = 'qrcode.png';
+      document.body.appendChild(link);
+      link.click();
+      document.body.removeChild(link);
+    }
+  };
+
+
+  const shareQRCode = () => {
+    // const qrcodeimg = generateQRCode();
+    if (navigator.share) {
+      fetch(`http://192.168.1.25/Zeal_Event/API/${qrimg}`)
+        .then((res) => res.blob())
+        .then((blob) => {
+          navigator.share({
+            title: 'My Zeal ID QR Code',
+            files: [new File([blob], 'qrcode.png', { type: 'image/png' })],
+          })
+            .then(() => console.log('QR code shared successfully!'))
+            .catch((error) => console.error('Error sharing QR code:', error));
+        })
+        .catch((error) => console.error('Error creating QR code file:', error));
+    } else if (/Android|iPhone|iPad/i.test(navigator.userAgent)) {
+      // const whatsappUrl = `https://wa.me/?te=${encodeURIComponent('Here is your QR code: ' + qrimg)}`;
+      // window.open(whatsappUrl, '_blank');
+      setShowShareButtons(true);
+    }
+  };
+
+
   return (
     <>
       <div>
-        {/* login htmlForm start here  */}
-        <div style={{ backgroundImage: `url(${Data.leftlogo})` }}>
-
-        </div>
-
-        {
-          !userexists && !modal && !submited && (
-            <div className="container" style={{ backgroundImage: `url(${Data.bgimg})` }}>
-              <div>
-                <form className='row'>
-                  <h3 className='text-center'>Facial Form</h3>
+        <div className="logo-container" style={{ backgroundImage: `url(${Data.backgroundImage})` }}>
+          <img src={leftlogo} />
+          {
+            !userexists && !modal && !submited && (
+              <div className="card ms-3 col-12 col-sm-10 col-md-8 col-lg-6">
+                <form className="row card-body">
+                  <h3 className="text-center">Facial Form</h3>
                   <div className="mb-3 col-md-12">
                     <label htmlFor="fname" className="form-label">First Name</label>
-                    <input type="text" className="form-control" id="f-name" name='fname' onChange={handleChange} />
-                    {nameError && <span className="error">{nameError}</span>}
+                    <input type="text" className="form-control" id="f-name" name="fname" onChange={handleChange} />
+                    {nameError && <span className="error text-danger">{nameError}</span>}
                   </div>
                   <div className="mb-3 col-md-12">
                     <label htmlFor="l-name" className="form-label">Last Name</label>
-                    <input type="text" className="form-control" id="lname" name='lname' onChange={handleChange} />
+                    <input type="text" className="form-control" id="lname" name="lname" onChange={handleChange} />
+                    {LastnameError && <span className="error text-danger">{LastnameError}</span>}
                   </div>
                   <div className="mb-3 col-md-12">
                     <label htmlFor="mobile-no" className="form-label">Mobile No</label>
-                    <input type="tel" className="form-control" id="mobileno" name='mobileno' onChange={handleChange} />
-                    {mobileError && <span className="error">{mobileError}</span>}
+                    <input type="tel" className="form-control" id="mobileno" name="mobileno" onChange={handleChange} />
+                    {mobileError && <span className="error text-danger">{mobileError}</span>}
                   </div>
                   <div className="mb-3 col-md-12">
                     <label htmlFor="email" className="form-label">Email</label>
-                    <input type="email" className="form-control" id="email" name='email' onChange={handleChange} />
-                    {emailError && <span className="error">{emailError}</span>}
+                    <input type="email" className="form-control" id="email" name="email" onChange={handleChange} />
+                    {emailError && <span className="error text-danger">{emailError}</span>}
                   </div>
-                  <button type="submit" className="btn btn-primary text-center" onClick={formSubmit}>Submit</button>
+                  <div className="mb-2 col-md-12 text-center">
+                    <button type="submit" className="btn btn-info text-center col-6" onClick={formSubmit}>Submit</button>
+                  </div>
                 </form>
               </div>
-            </div>
-          )
-        }
+            )
+          }
+        </div>
+
 
         {/* user already exists */}
+
         {
           userexists && (
             <>
-              <div className="container">
-                <div classNamename="alert alert-success" role="alert">
-                  <h4 className="alert-heading">Well done!</h4>
-                  <p>Aww yeah, you successfully read this important alert message. This example text is going to run a bit longer so that you can see how spacing within an alert works with this kind of content.</p>
-                  <hr />
-                  <p className="mb-0">Whenever you need to, be sure to use margin utilities to keep things nice and tidy.</p>
+              <div className="alert alert-danger alert-dismissible fade show" role="alert">
+                <strong>Error!</strong> Your Email and Mobile No are Already Used.
+                <div className="user-existsbtn text-center">
+                  <button type="button" className="btn btn-danger mt-2" onClick={showform}>Ok</button>
                 </div>
               </div>
-
             </>
           )
         }
 
         {
-          modal && (
-            <div>
-              <h2>open the modal box</h2>
-              <button onClick={handleConfirm}>ok</button>
-
+          modal && !IsModalVisible && (
+            <div className='modal-container'>
+              <h2>We would like your permission to take your photo for Registration. Do you consent to this?</h2>
+              <div className="btn-container">
+                <button onClick={handleConfirm} className='btn btn-info'>ok</button>
+                <button onClick={normalRegister} className='btn btn-info'>cancel</button>
+              </div>
             </div>
 
+          )
+        }
+
+        {IsModalVisible && (
+          <div className="modal-container text-center">
+            <h2 className='text-success'><span>Congratulations!</span> Thank you for your response...</h2>
+            <img src={` http://192.168.1.25/Zeal_Event/API/${qrimg}`} style={{ width: '150px', height: '150px' }} alt="QRCode " />
+            <div className="btn-container">
+              <button className='btn btn-info' onClick={downloadQr}>Download</button>
+              <button className='btn btn-info' onClick={shareQRCode}>Share</button>
+            </div>
+          </div>
+        )}
+
+        {
+          isLoading && (
+            <div className="loader"></div>
           )
         }
 
         {
           submited && (
             <>
-              <Screen zealid= {zealid} />
+              <Screen1 userdetails={userdetails} zealid={zealid} />
             </>
           )
         }
--- src/Components/Screen.jsx
+++ src/Components/Screen.jsx
@@ -1,36 +1,29 @@
 import { useRef, useEffect, useState } from 'react';
+import Webcam from 'react-webcam';
+import Data from '../Components/Data.json';
 import './Screen.css';
 
-function Screen({zealid}) {
-    const videoRef = useRef(null);
-    const canvasRef = useRef(null);
+let eid = `${Data.eid}`;
+
+function Screen({ userdetails, zealid }) {
+
+    const webcamRef = useRef();
+    const canvasRef = useRef();
     const [CapturedFace, setCapturedFace] = useState(null);
     const [captureCount, setCaptureCount] = useState(0);
+    const [uuid1, setUuid1] = useState(null);
+    const [uuid2, setUuid2] = useState(null);
+    const [uuid3, setUuid3] = useState(null);
     const [recapture, setRecapture] = useState(false);
     const [IsModalVisible, setIsModalVisible] = useState(false);
     const [isLoading, setIsLoading] = useState(false);
     const [faceborder, showfaceborder] = useState(false);
     const [moreface, setMoreface] = useState(false);
-    const [base64img, setBase64Image] = useState('')
+    const [qrimg, setQrimg] = useState('');
 
     useEffect(() => {
-        if (videoRef.current) {
-            startVideo();
-        }
-    }, [videoRef]); // Only runs when videoRef is available
-
-    const startVideo = () => {
-        navigator.mediaDevices.getUserMedia({ video: true })
-            .then((currentStream) => {
-                if (videoRef.current) {
-                    videoRef.current.srcObject = currentStream; // Set the video stream
-                    showfaceborder(true);
-                }
-            })
-            .catch((err) => {
-                console.error("Error accessing camera: ", err);
-            });
-    };
+        showfaceborder(true);
+    })
 
     const handleCaptureClick = () => {
         if (isLoading) return; // Prevent additional clicks while loading
@@ -42,126 +35,209 @@ function Screen({zealid}) {
             return;
         }
 
-        if (videoRef.current) {
-            const videoWidth = videoRef.current.videoWidth;
-            const videoHeight = videoRef.current.videoHeight;
+        if (webcamRef.current) {
+            const imageSrc = webcamRef.current.getScreenshot();
+            setCapturedFace(imageSrc);
 
-            const fullFrameCanvas = document.createElement('canvas');
-            fullFrameCanvas.width = videoWidth;
-            fullFrameCanvas.height = videoHeight;
+            // Convert data URL to blob
+            fetch(imageSrc)
+                .then(res => res.blob())
+                .then(blob => {
+                    const fullFrameImageFile = new File([blob], 'captured_full_image.png', { type: 'image/png' });
+                    const files = {
+                        "photos": fullFrameImageFile,
+                    };
+                    const data = {
+                        "zeal_id": zealid,
+                        "event_id": eid
+                    }
 
-            const fullFrameCtx = fullFrameCanvas.getContext('2d');
-            fullFrameCtx.drawImage(videoRef.current, 0, 0, videoWidth, videoHeight);
+                    const url = " http://192.168.1.25/Zeal_Event/API/enrollPerson.php";
+                    const formData = new FormData();
+                    Object.keys(files).forEach(key => formData.append(key, files[key]));
+                    Object.keys(data).forEach(key => formData.append(key, data[key]));
 
 
-            fullFrameCanvas.toBlob((blob) => {
-                const fullFrameImageFile = new File([blob], 'captured_full_image.png', { type: 'image/png' });
-
-                // const imageUrl = URL.createObjectURL(fullFrameImageFile);
-                // setCapturedFace(imageUrl);
-
-                const formData = new FormData();
-                formData.append('Subject_id', zealid);
-                formData.append('file', fullFrameImageFile,); // Attach the image file
-
-                fetch(`http://192.168.1.25/Zeal_Event/API/kairos.php`, {
-                    method: "POST",
-                    body: formData
-                })
-                    .then(response => response.json())
-                    .then(data => {
-                        console.log("Response data", data);
-
-                        if (data.Response.face_id) {
-                            setMoreface(true);
-                            setCaptureCount(prevCount => {
-                                const newCount = prevCount + 1;
-                                if (newCount === 1) {
-                                    setCaptureCount(1);
-
-                                } else if (newCount === 2) {
-                                    setCaptureCount(2);
-
-                                } else if (newCount === 3) {
-                                    setCaptureCount(3);
-                                }
-                                return newCount;
-                            });
-                        } else if (!data.Response.face_id) {
-                            setRecapture(true);
-                        }
+                    fetch(url, {
+                        method: "POST",
+                        body: formData
                     })
-                    .catch(error => console.error('An error occurred:', error))
-                    .finally(() => {
-                        setIsLoading(false);
-                    });
-            }, 'image/png');
+                        .then(response => response.json())
+                        .then(data => {
+                            console.log("Response data", data);
+                            if (data.status === 'success' && data.faces && data.faces.length > 0) {
+                                setIsLoading(false);
+                                const newUuid = data.uuid;
+                                setMoreface(true);
+                                setCaptureCount(prevCount => {
+                                    const newCount = prevCount + 1;
+                                    if (newCount === 1) {
+                                        setUuid1(newUuid);
+                                        console.log('UUID 1:', newUuid);
+                                    } else if (newCount === 2) {
+                                        setUuid2(newUuid);
+                                        console.log('UUID 2:', newUuid);
+                                    } else if (newCount === 3) {
+                                        setUuid3(newUuid);
+                                        console.log('UUID 3:', newUuid);
+                                    }
+                                    return newCount;
+                                });
+                            } else if (data.response.status === 'failure') {
+                                setIsLoading(false);
+                                setRecapture(true);
+                            }
+                        })
+                        .catch(error => console.error('An error occurred:', error))
+                        .finally(() => {
+                            setIsLoading(false);
+                        });
+                });
+        }
+    };
+
+    useEffect(() => {
+        if (zealid && uuid1 && uuid2 && uuid3) {
+            const eid = `${Data.eid}`;
+            const finalUrl = ` http://192.168.1.25/Zeal_Event/API/register.php`;
+            const formData = new FormData();
+            formData.append('first_name', userdetails.fname);
+            formData.append('last_name', userdetails.lname);
+            formData.append('contact', userdetails.mobileno);
+            formData.append('email', userdetails.email);
+            formData.append('zeal_id', zealid);
+            formData.append('eid', eid);
+            formData.append('type', 'str');
+            formData.append('uuid1', uuid1);
+            formData.append('uuid2', uuid2);
+            formData.append('uuid3', uuid3);
+
+            fetch(finalUrl, {
+                method: 'POST',
+                body: formData
+            })
+                .then(response => response.json())
+                .then(finalData => {
+                    console.log('Final API Response:', finalData);
+                    setQrimg(finalData.QR_FilePath);
+                    if (finalData.Store == true) {
+                        setIsModalVisible(true);
+                    }
+                })
+                .catch(error => console.error('Error posting final data:', error));
+        }
+    }, [zealid, uuid1, uuid2, uuid3]);
+
+
+    const downloadQr = () => {
+        if (qrimg) {
+            const link = document.createElement('a');
+            link.href = `http://192.168.1.25/Zeal_Event/API/${qrimg}`;
+            link.download = 'qrcode.png';
+            document.body.appendChild(link);
+            link.click();
+            document.body.removeChild(link);
+        }
+    };
+
+
+    const shareQRCode = () => {
+        // const qrcodeimg = generateQRCode();
+        if (navigator.share) {
+            fetch(`http://192.168.1.25/Zeal_Event/API/${qrimg}`)
+                .then((res) => res.blob())
+                .then((blob) => {
+                    navigator.share({
+                        title: 'My Zeal ID QR Code',
+                        files: [new File([blob], 'qrcode.png', { type: 'image/png' })],
+                    })
+                        .then(() => console.log('QR code shared successfully!'))
+                        .catch((error) => console.error('Error sharing QR code:', error));
+                })
+                .catch((error) => console.error('Error creating QR code file:', error));
+        } else if (/Android|iPhone|iPad/i.test(navigator.userAgent)) {
+            // const whatsappUrl = `https://wa.me/?te=${encodeURIComponent('Here is your QR code: ' + qrimg)}`;
+            // window.open(whatsappUrl, '_blank');
+            setShowShareButtons(true);
         }
     };
 
     return (
-        <div className="myapp">
-            <div className='username'>
-                <h1>Face Detection</h1>
-                <p>sagar shinde</p>
-            </div>
+        <>
+            <div className="myapp">
+                <div className='username'>
+                    <h1>Face Detection</h1>
+                    <p>{userdetails.fname}</p>
+                </div>
 
-            {!IsModalVisible && (
-                <div className="appvide">
-                    {isLoading ? (
-                        CapturedFace && <img src={CapturedFace} alt="Captured face" style={{ transform: 'scaleX(-1)' }} />
-                    ) : !isLoading &&(
-                        <>
-                            <video
-                                ref={videoRef}
-                                autoPlay
-                                playsInline
-                                crossOrigin="anonymous"
-                                style={{ transform: 'scaleX(-1)' }}
-                            />
-                            <canvas ref={canvasRef} className="appcanvas" />
-                            {faceborder && (
-                                <div className='face-border'>
-                                    <svg width="200" height="250">
-                                        <circle cx="200" cy="225" r="280" />
-                                    </svg>
+                {!IsModalVisible && (
+                    <div className="appvide">
+                        {isLoading ? (
+                            CapturedFace && <img src={CapturedFace} alt="Captured face" style={{ transform: 'scaleX(-1)' }} />
+                        ) : !isLoading && (
+                            <>
+                                <Webcam
+                                    audio={false}
+                                    ref={webcamRef}
+                                    screenshotFormat="image/png"
+                                    videoConstraints={{ facingMode: "user" }}
+                                    style={{ transform: 'scaleX(-1)' }}
+                                />
+                                <canvas ref={canvasRef} className="appcanvas" />
+                                {faceborder && (
+                                    <div className='face-border'>
+                                        <svg width="200" height="250">
+                                            <circle cx="200" cy="225" r="280" />
+                                        </svg>
+                                    </div>
+                                )}
+                            </>
+                        )}
+                    </div>
+                )}
+
+                {!IsModalVisible && (
+                    <div className="capturebtn">
+                        <button onClick={handleCaptureClick} disabled={isLoading}>
+                            {isLoading ? (
+                                <div>
+                                    <span></span>Capturing...
                                 </div>
-                            )}
-                        </>
-                    )}
-                </div>
-            )}
+                            ) : 'Capture'}
+                        </button>
+                    </div>
+                )}
 
-            {!IsModalVisible && (
-                <div className="capturebtn">
-                    <button onClick={handleCaptureClick} disabled={isLoading}>
-                        {isLoading ? <div><span></span>Capturing...</div> : 'Capture'}
-                    </button>
-                </div>
-            )}
+                {IsModalVisible && (
+                    <div className="showmassageface text-center">
+                        <h2 className='text-success'><span>Congratulations!</span> Thank you for your response...</h2>
+                        <img src={` http://192.168.1.25/Zeal_Event/API/${qrimg}`} style={{ width: '150px', height: '150px' }} alt="QRCode " />
+                        <div className="btn-container">
+                            <button className='btn btn-info' onClick={downloadQr}>Download</button>
+                            <button className='btn btn-info' onClick={shareQRCode}>Share</button>
+                        </div>
+                    </div>
+                )}
 
-            {IsModalVisible && (
-                <div className="showmassageface">
-                    <h2><span>Congratulations!</span> Thank you for your response....</h2>
-                </div>
-            )}
+                {isLoading && (
+                    <div className="loader"></div>
+                )}
 
-            {isLoading && <div className="loader"></div>}
+                {moreface && (
+                    <div className='modal-capture'>
+                        <h3><span>Congratulations!!</span> Your face {captureCount} image is stored successfully</h3>
+                        <button onClick={() => setMoreface(false)}>OK</button>
+                    </div>
+                )}
 
-            {moreface && (
-                <div className='modal-capture'>
-                    <h3><span>Congratulations!!</span> Your face image {captureCount} is stored successfully </h3>
-                    <button onClick={() => setMoreface(false)}>Ok</button>
-                </div>
-            )}
-
-            {recapture && !isLoading && (
-                <div className='showrecapture'>
-                    <h3>Your face is not captured properly. Please recapture again.</h3>
-                    <button onClick={() => setRecapture(false)}>Recapture</button>
-                </div>
-            )}
-        </div>
+                {recapture && !isLoading && (
+                    <div className='showrecapture'>
+                        <h3>Your face was not captured properly, please recapture again</h3>
+                        <button onClick={() => setRecapture(false)}>Recapture</button>
+                    </div>
+                )}
+            </div>
+        </>
     );
 }
 
